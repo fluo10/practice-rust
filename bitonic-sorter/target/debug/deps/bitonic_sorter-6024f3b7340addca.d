1f1c8f7c2c9724679e84480e008755c1bbd16506
fwtn <fwtn+git@fireturtle.net>
2020-02-24 09:50:54 +0900

    Test second.rs Completed.

--- bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.d
+++ bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.d
@@ -1,6 +1,7 @@
-/mnt/z/Documents/Develop/practice-rust/bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.rmeta: src/lib.rs src/first.rs
+/mnt/c/Users/fmtfl/OneDrive/ドキュメント/Develop/practice-rust/bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.rmeta: src/lib.rs src/first.rs src/second.rs
 
-/mnt/z/Documents/Develop/practice-rust/bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.d: src/lib.rs src/first.rs
+/mnt/c/Users/fmtfl/OneDrive/ドキュメント/Develop/practice-rust/bitonic-sorter/target/debug/deps/bitonic_sorter-6024f3b7340addca.d: src/lib.rs src/first.rs src/second.rs
 
 src/lib.rs:
 src/first.rs:
+src/second.rs:
